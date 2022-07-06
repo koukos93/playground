0744b6719fbce7f1fd4826322dab6d1db39d23ce
Koukos93 <alekos93@yahoo.gr>
2022-07-06 21:14:23 +0300

    Renaming variables for readability

--- main.py
+++ main.py
@@ -1,40 +1,126 @@
-from datetime import datetime
-from backend import Car, find_time_stayed
+import sqlite3
+import datetime as dt
+import time
+import tkinter.messagebox
+from tkinter import *
+import math
 
 
-parking_open = True
-car_dict = {}
-# object test car for testing purposes. Remove before deploy
-test_car = Car(datetime.now().strftime("%H:%M"), "arm4567")
-car_dict["test_plate"] = test_car
+def insert_car():
+    """
+    Insert record in the database
+
+    :return: None
+    """
+    conn = sqlite3.connect("Cars.db")
+    cursor = conn.cursor()
+    time_now = dt.datetime.now().strftime("%Y-%m-%d %H:%M")
+    #get user input from entry box and insert record
+    plate_num = entry_box.get()
+    entry_box.delete(0, "end")
+    cursor.execute(f'INSERT INTO customers VALUES ("{plate_num}", "{time_now}")')
+    conn.commit()
+    conn.close()
+    return None
 
 
+def delete_car():
+    """
+    Remove record from database based on user input
 
-def parking():
+    :return: None
+    """
+    car_plate = exit_entry.get()
+    exit_entry.delete(0, "end")
+    conn = sqlite3.connect("Cars.db")
+    cursor = conn.cursor()
+    if tkinter.messagebox.askokcancel(message="Delete car from garage?"):
+        cursor.execute(f'DELETE from customers WHERE plate_number = "{car_plate}"')
+    conn.commit()
+    conn.close()
+    return None
 
-    while parking_open:
-        # enter cars in the parking lot
-        enter_plate = input("Enter Plate to enter parking lot: ").lower()
-        # Name of the key is the user input which is the Plate of the car and the value is the time_in.
-        if enter_plate in car_dict.keys():
-            print("car already in the parking lot")
-            # recursion to start again if the car already is in the parking lot
-            parking()
-        # take input and make it a key in a dict. then assign the current time as
-        # the value of that key. So that creates a dict: {plate:time_in}
-        car_dict[enter_plate] = Car(datetime.now().strftime("%H:%M"), enter_plate)
-        print(car_dict)
-        # exit cars from parking lot
-        exit_car = input("enter car to exit: ").lower()
-        if exit_car in car_dict.keys():
-            car_time_in = car_dict[exit_car].in_time
-            print(car_time_in)
-            print(int(find_time_stayed(car_time_in)))
-            del car_dict[exit_car]
-            # here I must compare the in_time with the current time and find the difference in minutes
-            # then create a function to get the minutes that car stayed and return the price
+
+def exit_car_from_garage():
+    """
+    Query the database based on user input
+
+    :return: None
+    """
+    # db connection
+    conn = sqlite3.connect("Cars.db")
+    cursor = conn.cursor()
+    # get user input from entry box and fetch the record
+    car_plate = exit_entry.get()
+    cursor.execute(f'SELECT * FROM customers WHERE plate_number = "{car_plate}"')
+    items = cursor.fetchone()
+
+    # getting the time from database(string) and
+    # converting it to datetime object to be able to compare with current time
+    time_in = dt.datetime.strptime(items[1], "%Y-%m-%d %H:%M")
+    difference = dt.datetime.now() - time_in
+    time_dif_in_minutes = int(difference.total_seconds() / 60)
+    # calculating the time stayed and price and display to the user
+    hours, minutes = divmod(time_dif_in_minutes, 60)
+    tkinter.messagebox.showinfo(title="Χρονος Παραμονης",
+                                message=f"{hours}ω και {minutes}λ\n"
+                                        f"Χρεωση: {parking_fee(time_dif_in_minutes)}")
+
+    conn.commit()
+    conn.close()
+    return None
+
+
+def parking_fee(difference_minutes):
+    """
+    Calculates the parking cost
+
+    :param difference_minutes: int
+
+    :return: Cost of parking -- int
+    """
+    first_minutes = 1
+    first_hour = 4.5
+    other_hours = 2
+    minimum_fee = 3
+    fee = minimum_fee
+    # if time stayed less than 15 minutes then price is minimum price of 3$
+
+    if difference_minutes > 15:
+        hours, minutes = divmod(difference_minutes, 60)
+        # Variable to subtract the first hour that we already charged and multiply other_hours for
+        # the rest of the hours
+        hours_calculator = hours - 1
+        # If we have 0 hours and more than 15 minutes, charge is the first hour charge.
+        if hours_calculator < 0:
+            fee = first_hour
+            return fee
+        fee = first_hour + hours_calculator * other_hours
+        if 0 <= minutes < 11:
+            return fee
+        elif 11 <= minutes <= 15:
+            fee += first_minutes
         else:
-            print("No car with this licence plate")
+            fee += other_hours
+    return fee
 
 
-parking()
+# --------------------------- GUI-------------------------------#
+
+
+root = Tk()
+root.title("ParkingSpot")
+root.minsize(500, 500)
+# show and pack buttons and entries
+entry_box = Entry()
+entry_box.pack()
+enter_button = Button(text="Entry", command=insert_car)
+exit_entry = Entry()
+exit_button = Button(text="Exit", command=lambda: [exit_car_from_garage(),
+                                                   delete_car()])
+enter_button.pack()
+exit_entry.pack()
+exit_button.pack()
+
+
+root.mainloop()
--- testing.py
+++ testing.py
@@ -1,34 +1,64 @@
-from datetime import datetime
+# from datetime import datetime
+#
+#
+# # this snippet checks if a key is in a dictionary.
+#
+# #if enter_plate in car_dict.keys():
+#     #print("car already in the parking lot")
+#
+# # input time(time1) for this function should be the time_in of a car I want to exit from parking lot
+# def find_time_stayed(time1):
+#     """Takes one instance of time and returns the difference from
+#     the given time and the current time"""
+#     time2 = datetime.now().strftime("%H:%M")
+#     print(type(time2))
+#     FMT = "%H:%M"
+#     time_passed = datetime.strptime(time2, FMT) - datetime.strptime(time1, FMT)
+#     return time_passed.total_seconds() / 60
+#
+#
+# # Pricing conditions
+# time = int(find_time_stayed("18:10"))
+# print(time)
+# if time <= 11:
+#     print("The price is 3 euros")
+# elif time <= 70:
+#     print("The price is 4.5 euros")
+# elif time <= 75:
+#     print("The price is 5.5 euros")
+# elif time <= 130:
+#     print("The price is 6.5 euros")
+# elif time <= 190:
+#     print("The price is 7.5 euros")
+# elif time <= 195:
+#     print("The price is 8.5 euros")
 
 
-# this snippet checks if a key is in a dictionary.
+def parking_fee(difference_minutes):
+    first_minutes = 1
+    first_hour = 4.5
+    other_hours = 2
+    minimum_fee = 3
+    fee = minimum_fee
+    # if time stayed less than 15 minutes then price is minimum price of 3$
 
-#if enter_plate in car_dict.keys():
-    #print("car already in the parking lot")
+    if difference_minutes > 15:
+        hours, minutes = divmod(difference_minutes, 60)
+        # Variable to subtract the first hour that we already charged and multiply other_hours for
+        # the rest of the hours
+        hours_calculator = hours - 1
+        # If we have 0 hours and more than 15 minutes, charge is the first hour charge.
+        if hours_calculator < 0:
+            fee = first_hour
+            return fee
+        fee = first_hour + hours_calculator * other_hours
+        if 0 <= minutes < 11:
+            return fee
+        elif 11 <= minutes <= 15:
+            fee += first_minutes
+        else:
+            fee += other_hours
+    return fee
 
-# input time(time1) for this function should be the time_in of a car I want to exit from parking lot
-def find_time_stayed(time1):
-    """Takes one instance of time and returns the difference from
-    the given time and the current time"""
-    time2 = datetime.now().strftime("%H:%M")
-    print(type(time2))
-    FMT = "%H:%M"
-    time_passed = datetime.strptime(time2, FMT) - datetime.strptime(time1, FMT)
-    return time_passed.total_seconds() / 60
 
-
-# Pricing conditions
-time = int(find_time_stayed("18:10"))
-print(time)
-if time <= 11:
-    print("The price is 3 euros")
-elif time <= 70:
-    print("The price is 4.5 euros")
-elif time <= 75:
-    print("The price is 5.5 euros")
-elif time <= 130:
-    print("The price is 6.5 euros")
-elif time <= 190:
-    print("The price is 7.5 euros")
-elif time <= 195:
-    print("The price is 8.5 euros")
+print(parking_fee(16))
